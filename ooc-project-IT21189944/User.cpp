a1a9cdf817ab2aa3f9d19b8f7c35f5c714d4d923
Chillbroh <>
2022-05-18 16:33:17 +0000

    I implemented the user part

---
+++ ooc-project-IT21189944/User.cpp
@@ -0,0 +1,26 @@
+//User.cpp
+#include<iostream>
+#include "User.h"
+#include<cstring>
+using namespace std;
+
+User::User(){
+  strcpy(userName, "");
+	strcpy(password, "");
+}
+User::User(char pusername[], char pass[]) {
+	strcpy(userName, pusername);
+	strcpy(password, pass);
+}
+void User::display (){
+  cout << userName << endl << password << endl;
+}
+void User::login() {
+
+}
+void User::validateUser() {
+
+}
+User::~User() {
+	
+}
---
+++ ooc-project-IT21189944/User.h
@@ -0,0 +1,16 @@
+//Class User
+#pragma once
+class User
+{
+protected:
+	char userName[30];
+	char password[15];
+
+public:
+  User();
+	User(char pusername[], char pass[]);
+  void display();
+	void login();
+	void validateUser();
+	~User();
+};
